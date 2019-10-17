601dd74ce43804266d70c0460569eaa274cbf7fc
johnabele <jdabele@gmail.com>
2019-10-17 01:05:37 -0500

    Add files via upload

---
+++ Models/ResourceAlert.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace processCreateDeleteAlerts.Models
+{
+    public class ResourceAlert
+    {
+        public string id { get; set; }
+        public DateTime eventTime { get; set; } 
+        public data data { get; set; }
+    }
+
+    public class data
+    {
+        public string resourceId { get; set; }
+        public string resourceName { get; set; }
+        public string resourceGroup { get; set; }
+        public string resourceUri { get; set; }
+        public string subscriptionId { get; set; }
+        public string status { get; set; }
+        public string resourceType { get; set; }
+        public string currentHealthStatus { get; set; }
+        public string previousHealthStatus { get; set; }
+        public string operationName { get; set; }
+        public string location { get; set; }
+    }
+}
---
+++ Util/AlertHelper.cs
@@ -0,0 +1,39 @@
+﻿using processCreateDeleteAlerts.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace processCreateDeleteAlerts.Util
+{
+    public static class AlertHelper
+    {
+        public static ResourceAlert PopulateMetaData(ResourceAlert resource)
+        {
+            resource.data.resourceId = resource.data.resourceUri;
+            resource.data.resourceName = resource.data.resourceUri.Split('/')[8];
+            resource.data.resourceGroup = resource.data.resourceUri.Split('/')[4];
+            resource.id = resource.data.subscriptionId + "_" + resource.data.resourceGroup + "_" + resource.data.resourceName;
+
+            return resource;
+        }
+
+        public static ResourceAlertDTO ConvertToDTO(ResourceAlert resource)
+        {
+            var resourceAlertDTO = new ResourceAlertDTO()
+            {
+                alertId = Guid.NewGuid().ToString(),
+                alertStatus = "Resolved",
+                currentHealthStatus = "Available",
+                previousHealthStatus = "null",
+                eventTimestamp = resource.eventTime,
+                resourceId = resource.data.resourceUri,
+                id = Guid.NewGuid().ToString(),
+                resourceType = resource.data.resourceType,
+                subscriptionId = resource.data.subscriptionId
+            };
+
+            return resourceAlertDTO;
+        }
+    }
+}
